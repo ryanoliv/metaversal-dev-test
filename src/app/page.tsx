36368157f23ce6e395ad42673a63cc16b58679ba
Ryan Oliver <ryanandrewoliver@gmail.com>
2024-10-01 10:05:41 +0200

    static user feed page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,240 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section className="flex flex-col gap-12 py-8 px-4 bg-content/base">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-lg">Suggested Posts</h2>
+        <div className="flex flex-col bg-content/surface shadow rounded-2xl border border-content/border">
+          <div className="flex flex-col p-4 rounded-t-2xl">
+            <div className="flex gap-3">
+              <div>
+                <Image
+                  src="/Avatar.png"
+                  alt="User avatar"
+                  width={40}
+                  height={40}
+                  className="rounded-3xl"
+                />
+              </div>
+              <div className="flex flex-1 flex-col gap-3">
+                <div className="flex flex-col gap-1 py-1">
+                  <h3 className="text-base text-text/text-primary">
+                    Emily Johnson
+                  </h3>
+                  <p className="text-xs text-text/text-secondary">@emilys</p>
+                </div>
+                <p className="text-sm text-text/text-secondary">
+                  Post body lorem ipsum dolor sit amet consectetur. Sem
+                  vestibulum massa lacus interdum enim fringilla.
+                </p>
+                <div className="flex gap-3 text-primary/default">
+                  <span className="text-xs">#tag1</span>
+                  <span className="text-xs">#tag2</span>
+                  <span className="text-xs">#tag3</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-6 p-4 border-t rounded-b-2xl">
+            <span className="flex items-center gap-1 text-sm text-text/text-secondary">
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="text-text/text-light"
+              >
+                <path
+                  d="M4.66659 14.6666H2.66659C2.31296 14.6666 1.97382 14.5262 1.72378 14.2761C1.47373 14.0261 1.33325 13.6869 1.33325 13.3333V8.66665C1.33325 8.31302 1.47373 7.97389 1.72378 7.72384C1.97382 7.47379 2.31296 7.33331 2.66659 7.33331H4.66659M9.33325 5.99998V3.33331C9.33325 2.80288 9.12254 2.29417 8.74747 1.9191C8.37239 1.54403 7.86369 1.33331 7.33325 1.33331L4.66659 7.33331V14.6666H12.1866C12.5081 14.6703 12.8202 14.5576 13.0652 14.3493C13.3102 14.1411 13.4717 13.8513 13.5199 13.5333L14.4399 7.53331C14.4689 7.34222 14.456 7.1471 14.4021 6.96148C14.3483 6.77586 14.2547 6.60418 14.1278 6.45832C14.001 6.31247 13.844 6.19593 13.6676 6.11679C13.4913 6.03765 13.2999 5.99779 13.1066 5.99998H9.33325Z"
+                  stroke="currentColor"
+                  strokeWidth="1.33333"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              20
+            </span>
+            <span className="flex items-center gap-1 text-sm text-text/text-secondary">
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="text-text/text-light"
+              >
+                <path
+                  d="M14.6666 1.33331L7.33325 8.66665M14.6666 1.33331L9.99992 14.6666L7.33325 8.66665M14.6666 1.33331L1.33325 5.99998L7.33325 8.66665"
+                  stroke="currentColor"
+                  strokeWidth="1.33333"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              24
+            </span>
+            <span className="flex items-center gap-1 text-sm text-text/text-secondary">
+              <svg
+                width="14"
+                height="10"
+                viewBox="0 0 16 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="text-text/text-light"
+              >
+                <path
+                  d="M0.666748 6.00002C0.666748 6.00002 3.33341 0.666687 8.00008 0.666687C12.6667 0.666687 15.3334 6.00002 15.3334 6.00002C15.3334 6.00002 12.6667 11.3334 8.00008 11.3334C3.33341 11.3334 0.666748 6.00002 0.666748 6.00002Z"
+                  stroke="currentColor"
+                  strokeWidth="1.33333"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M8.00008 8.00002C9.10465 8.00002 10.0001 7.10459 10.0001 6.00002C10.0001 4.89545 9.10465 4.00002 8.00008 4.00002C6.89551 4.00002 6.00008 4.89545 6.00008 6.00002C6.00008 7.10459 6.89551 8.00002 8.00008 8.00002Z"
+                  stroke="#5C6970"
+                  strokeWidth="1.33333"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              1,230
+            </span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h2 className="text-lg">Who to follow</h2>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col bg-content/surface shadow rounded-2xl border border-content/border">
+            <div className="flex flex-col p-4 rounded-t-2xl">
+              <div className="flex gap-3">
+                <div>
+                  <Image
+                    src="/Avatar.png"
+                    alt="User avatar"
+                    width={40}
+                    height={40}
+                    className="rounded-3xl"
+                  />
+                </div>
+                <div className="flex flex-1 flex-col gap-3">
+                  <div className="flex flex-col gap-1 py-1">
+                    <h3 className="text-base text-text/text-primary">
+                      Emily Johnson
+                    </h3>
+                    <p className="text-xs text-text/text-secondary">@emilys</p>
+                  </div>
+                </div>
+                <button className="py-2 px-3.5 border text-primary/default border-primary/default rounded-3xl">
+                  Follow
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h2 className="text-lg">Recent</h2>
+        <div className="flex flex-col bg-content/surface shadow rounded-2xl border border-content/border">
+          <div className="flex flex-col p-4 rounded-t-2xl">
+            <div className="flex gap-3">
+              <div>
+                <Image
+                  src="/Avatar.png"
+                  alt="User avatar"
+                  width={40}
+                  height={40}
+                  className="rounded-3xl"
+                />
+              </div>
+              <div className="flex flex-1 flex-col gap-3">
+                <div className="flex flex-col gap-1 py-1">
+                  <h3 className="text-base text-text/text-primary">
+                    Emily Johnson
+                  </h3>
+                  <p className="text-xs text-text/text-secondary">@emilys</p>
+                </div>
+                <p className="text-sm text-text/text-secondary">
+                  Post body lorem ipsum dolor sit amet consectetur. Sem
+                  vestibulum massa lacus interdum enim fringilla.
+                </p>
+                <div className="flex gap-3 text-primary/default">
+                  <span className="text-xs">#tag1</span>
+                  <span className="text-xs">#tag2</span>
+                  <span className="text-xs">#tag3</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-6 p-4 border-t rounded-b-2xl">
+            <span className="flex items-center gap-1 text-sm text-text/text-secondary">
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="text-text/text-light"
+              >
+                <path
+                  d="M4.66659 14.6666H2.66659C2.31296 14.6666 1.97382 14.5262 1.72378 14.2761C1.47373 14.0261 1.33325 13.6869 1.33325 13.3333V8.66665C1.33325 8.31302 1.47373 7.97389 1.72378 7.72384C1.97382 7.47379 2.31296 7.33331 2.66659 7.33331H4.66659M9.33325 5.99998V3.33331C9.33325 2.80288 9.12254 2.29417 8.74747 1.9191C8.37239 1.54403 7.86369 1.33331 7.33325 1.33331L4.66659 7.33331V14.6666H12.1866C12.5081 14.6703 12.8202 14.5576 13.0652 14.3493C13.3102 14.1411 13.4717 13.8513 13.5199 13.5333L14.4399 7.53331C14.4689 7.34222 14.456 7.1471 14.4021 6.96148C14.3483 6.77586 14.2547 6.60418 14.1278 6.45832C14.001 6.31247 13.844 6.19593 13.6676 6.11679C13.4913 6.03765 13.2999 5.99779 13.1066 5.99998H9.33325Z"
+                  stroke="currentColor"
+                  stroke-width="1.33333"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              20
+            </span>
+            <span className="flex items-center gap-1 text-sm text-text/text-secondary">
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="text-text/text-light"
+              >
+                <path
+                  d="M14.6666 1.33331L7.33325 8.66665M14.6666 1.33331L9.99992 14.6666L7.33325 8.66665M14.6666 1.33331L1.33325 5.99998L7.33325 8.66665"
+                  stroke="currentColor"
+                  stroke-width="1.33333"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              24
+            </span>
+            <span className="flex items-center gap-1 text-sm text-text/text-secondary">
+              <svg
+                width="14"
+                height="10"
+                viewBox="0 0 16 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="text-text/text-light"
+              >
+                <path
+                  d="M0.666748 6.00002C0.666748 6.00002 3.33341 0.666687 8.00008 0.666687C12.6667 0.666687 15.3334 6.00002 15.3334 6.00002C15.3334 6.00002 12.6667 11.3334 8.00008 11.3334C3.33341 11.3334 0.666748 6.00002 0.666748 6.00002Z"
+                  stroke="currentColor"
+                  stroke-width="1.33333"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M8.00008 8.00002C9.10465 8.00002 10.0001 7.10459 10.0001 6.00002C10.0001 4.89545 9.10465 4.00002 8.00008 4.00002C6.89551 4.00002 6.00008 4.89545 6.00008 6.00002C6.00008 7.10459 6.89551 8.00002 8.00008 8.00002Z"
+                  stroke="#5C6970"
+                  stroke-width="1.33333"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              1,230
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,56 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "white/100": "#fff",
+        "content/surface": "#fff",
+        "content/base": "#F9FAFA",
+        "content/border": "#E4E7E8",
+        "text/text-primary": "#141C24",
+        "text/text-secondary": "#5C6970",
+        "text/text-light": "#5C6970",
+        "primary/default": "#4426D9",
+        "slate/15": "#DFDFDF",
+      },
+      fontSize: {
+        xs: [
+          "0.75rem",
+          {
+            lineHeight: "0.87875rem",
+          },
+        ],
+        sm: [
+          "0.875rem",
+          {
+            lineHeight: "1.025625rem",
+          },
+        ],
+        base: [
+          "1rem",
+          {
+            lineHeight: "1rem",
+          },
+        ],
+        md: [
+          "1.125rem",
+          {
+            lineHeight: "1.125rem",
+            fontWeight: "800",
+          },
+        ],
+        lg: [
+          "1.5rem",
+          {
+            lineHeight: "1.5rem",
+            fontWeight: "800",
+          },
+        ],
+        xl: [
+          "1.875rem",
+          {
+            lineHeight: "1.875rem",
+            fontWeight: "800",
+          },
+        ],
       },
     },
   },
